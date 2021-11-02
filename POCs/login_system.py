efaba769591058db20e25983ba1dc70dad3ee8c8
doriav20 <86165796+doriav20@users.noreply.github.com>
2021-11-02 19:00:27 +0200

    Update login_system.py

--- POCs/login_system.py
+++ POCs/login_system.py
@@ -1,21 +1,22 @@
 from password_generator import generate_password
 from email_sender import send_email
 
-# email_address = input('Enter your email address: ')
-# generate_pass = input('Do you want to generate password? Y/N').lower() == 'y'
+if __name__ == '__main__':
+    # email_address = input('Enter your email address: ')
+    # generate_pass = input('Do you want to generate password? Y/N').lower() == 'y'
 
-email_address = "***User's Email Here***"
-generate_pass = False
+    email_address = "***User's Email Here***"
+    generate_pass = False
 
-if generate_pass:
-    length = 8
-    password = generate_password(length, include_uppercase=False, include_numbers=True, include_symbols=False)
-else:
-    password = input('Choose password: ')
+    if generate_pass:
+        length = 8
+        password = generate_password(length, include_uppercase=False, include_numbers=True, include_symbols=False)
+    else:
+        password = input('Choose password: ')
 
-print(f'Login info:\n\tEmail: {email_address}\n\tPassword: {password}')
+    print(f'Login info:\n\tEmail: {email_address}\n\tPassword: {password}')
 
-SUBJECT = 'We are glad you have joined us - FZIP Locker'
-BODY = 'Hey dear, your login details are:\n\tUsername\Email: {0}\n\tPassword: {1}\nHope you will enjoy...\nFZIP Locker'
-send_email(
-    ["***Mail Domain's Username***", "***Mail Domain's Password***"], email_address, SUBJECT, BODY.format(email_address, password))
+    SUBJECT = 'We are glad you have joined us - FZIP Locker'
+    BODY = 'Hey dear, your login details are:\n\tUsername\Email: {0}\n\tPassword: {1}\nHope you will enjoy...\nFZIP Locker'
+    send_email(
+        ["***Mail Domain's Username***", "***Mail Domain's Password***"], email_address, SUBJECT, BODY.format(email_address, password))
