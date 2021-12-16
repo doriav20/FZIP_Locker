bb22da9cb777c447dd477c7d82aabf819a0ae1e6
doriav20 <86165796+doriav20@users.noreply.github.com>
2021-12-16 16:23:06 +0200

    Add files via upload

--- Project/Client/ZIP_manager.py
+++ Project/Client/ZIP_manager.py
@@ -3,14 +3,15 @@ import zipfile
 from os import remove, mkdir, rmdir
 from os.path import exists, isdir
 import glob
-
 import pyminizip
-from Common.details_generator import generate_unique_filename
+from typing import List
+
+from Common.details_generator import generate_unique_path
 
 
 class ZIPManager:
     @staticmethod
-    def __compress_zipfile(src: [], dest: str) -> None:
+    def __compress_zipfile(src: List[str], dest: str) -> None:
         zf = zipfile.ZipFile(dest, mode='w', compression=zipfile.ZIP_DEFLATED)
         for filepath in src:
             if exists(filepath):
@@ -28,33 +29,39 @@ class ZIPManager:
             pyminizip.compress(src, None, dest, pwd, 0)
 
     @staticmethod
-    def compress(src: [], dest: str, pwd: str = None) -> None:
-        filename = generate_unique_filename('tmp')
+    def compress(src: List[str], dest: str, pwd: str = None) -> None:
+        filename = generate_unique_path('in')
         ZIPManager.__compress_zipfile(src=src, dest=filename)
         ZIPManager.__compress_pyminizip(src=filename, dest=dest, pwd=pwd)
         remove(filename)
+
+    @staticmethod
+    def __decompress_zipfile(src: str, dest: str) -> None:
+        with zipfile.ZipFile(src, mode='r') as zf:
+            zf.extractall(dest)
+
+    @staticmethod
+    def __decompress_pyminizip(src: str, dest: str, pwd: str) -> None:
+        pyminizip.uncompress(src, pwd, dest, 0)
 
     @staticmethod
     def decompress(path: str, pwd: str = None) -> None:
         if not exists(path):
             return
 
-        if (idx := path.find('.')) != -1:
-            dest = path[:idx] + '/'
+        if (index := path.rfind('.')) != -1 and path[index + 1] not in ['/', '\\']:
+            dest = path[:index] + '/'
         else:
-            return
+            dest = path + '_/'
 
-        dir_name = generate_unique_filename()
+        dir_name = generate_unique_path()
         mkdir(dir_name)
-        pyminizip.uncompress(path, pwd, dir_name, 0)
+        ZIPManager.__decompress_pyminizip(path, dir_name, pwd)
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
         filename = glob.glob(dir_name + '/**')[0]
 
-        zf = zipfile.ZipFile(filename, mode='r')
-        zf.extractall(dest)
-        zf.close()
+        ZIPManager.__decompress_zipfile(filename, dest)
 
         remove(filename)
         rmdir(dir_name)
-
