c480adf1cd7adf77e54992e76d6597b6fa2fbb6c
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-05 01:18:00 +0200

    Update details_generator.py

--- Project/Common/details_generator.py
+++ Project/Common/details_generator.py
@@ -9,10 +9,17 @@ LOWERCASE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q
 chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
          'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+ALL = ['!', '@', '#', '$', '*', '+', '-', '.', '_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C',
+       'D',
+       'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z']
+
 
 # 0 ; o ; O ; 1 ; i ; I ; l not included
 
-def generate_password(length, include_uppercase=True, include_numbers=True, include_symbols=True):
+def generate_password(length: int, include_uppercase: bool = True, include_numbers: bool = True,
+                      include_symbols: bool = True) -> str:
     _chars = []
     _chars.extend(LOWERCASE)
     if include_uppercase:
@@ -25,6 +32,15 @@ def generate_password(length, include_uppercase=True, include_numbers=True, incl
     return password
 
 
+def valid_password(passowrd: str) -> bool:
+    if not (6 <= len(passowrd) <= 32):
+        return False
+    for ch in passowrd:
+        if ch not in ALL:
+            return False
+    return True
+
+
 def generate_unique_id(length: int) -> str:
     unique_id = ''
     length += ((-length) % 4)  # padding
@@ -35,10 +51,8 @@ def generate_unique_id(length: int) -> str:
     return unique_id[1:]
 
 
-def generate_unique_filename(extension: str = '') -> str:
+def generate_unique_path(extension: str = '') -> str:
     if extension:
         return generate_unique_id(16) + '.' + extension
     else:
         return generate_unique_id(16)
-
-
