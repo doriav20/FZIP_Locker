61fe3eb91484548268e4d47ad2a6ad92dd3cb61f
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-05 01:16:12 +0200

    Add files via upload

---
+++ Project/Client/face_detector.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+import numpy as np
+from cv2 import cv2
+
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_alt2.xml')
+
+
+def detect_face() -> Tuple[np.ndarray, np.ndarray]:
+    cap = cv2.VideoCapture(0)
+    roi_gray = None
+    roi_preview = None
+    times = 0
+    while times < 3:
+        ret, frame = cap.read()
+        frame_h = frame.shape[0]
+        frame_w = frame.shape[1]
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            if not (w >= 200 and h >= 200):
+                continue
+            roi_gray = gray[y:y + h, x:x + w]  # (ycord_start, ycord_end)
+
+            roi_preview = frame[max(y - 50, 0):min(y + h + 50, frame_h), max(x - 50, 0):min(x + w + 50, frame_w)]
+
+            times += 1
+
+        cv2.imshow('Look at the camera', frame)
+        cv2.waitKey(20)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return roi_gray, roi_preview
