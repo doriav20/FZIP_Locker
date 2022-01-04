4098f4ee81316933a39b80005f1697b65defe3f9
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-05 01:13:58 +0200

    Update ZIP_manager.py

--- Project/Client/ZIP_manager.py
+++ Project/Client/ZIP_manager.py
@@ -1,39 +1,70 @@
 import os
+import os
+import shutil
 import zipfile
-from os import remove, mkdir, rmdir
-from os.path import exists, isdir
+from shutil import rmtree
 import glob
 import pyminizip
-from typing import List
+from typing import Tuple
 
 from Common.details_generator import generate_unique_path
+from Common.operation_result import OperationResultType
 
 
 class ZIPManager:
     @staticmethod
-    def __compress_zipfile(src: List[str], dest: str) -> None:
+    def __compress_zipfile(src: Tuple[str, ...], dest: str) -> None:
         zf = zipfile.ZipFile(dest, mode='w', compression=zipfile.ZIP_DEFLATED)
         for filepath in src:
-            if exists(filepath):
-                if isdir(filepath):
-                    dir_files = glob.glob(filepath + '/**', recursive=True)
-                    for fpath in dir_files:
-                        zf.write(fpath, compress_type=zipfile.ZIP_DEFLATED)
+            if os.path.exists(filepath):
+                if os.path.isdir(filepath):
+                    absolute_root_part = filepath.rstrip('\\')
+                    absolute_root_part = absolute_root_part[:absolute_root_part.rfind('\\')] + '\\'
+
+                    dir_files = glob.glob(filepath + '\\**', recursive=True)
+                    aliases = [fpath[len(absolute_root_part):] for fpath in dir_files]
+
+                    for fpath, alias in zip(dir_files, aliases):
+                        zf.write(fpath, arcname=alias, compress_type=zipfile.ZIP_DEFLATED)
                 else:
-                    zf.write(filepath, compress_type=zipfile.ZIP_DEFLATED)
+                    alias = filepath
+                    if '\\' in alias:
+                        alias = (alias.rstrip('\\'))
+                        alias = alias[alias.rfind('\\') + 1:]
+                    if '/' in alias:
+                        alias = (alias.rstrip('/'))
+                        alias = alias[alias.rfind('/') + 1:]
+                    print(alias)
+                    zf.write(filepath, arcname=alias, compress_type=zipfile.ZIP_DEFLATED)
         zf.close()
 
     @staticmethod
     def __compress_pyminizip(src: str, dest: str, pwd: str = None) -> None:
-        if exists(src):
+        if os.path.exists(src):
             pyminizip.compress(src, None, dest, pwd, 0)
 
     @staticmethod
-    def compress(src: List[str], dest: str, pwd: str = None) -> None:
-        filename = generate_unique_path('in')
-        ZIPManager.__compress_zipfile(src=src, dest=filename)
-        ZIPManager.__compress_pyminizip(src=filename, dest=dest, pwd=pwd)
-        remove(filename)
+    def compress(src: Tuple[str, ...], dest: str, pwd: str = None, delete_src: bool = False) -> OperationResultType:
+        filename = ''
+        try:
+            filename = generate_unique_path('in')
+            ZIPManager.__compress_zipfile(src=src, dest=filename)
+            ZIPManager.__compress_pyminizip(src=filename, dest=dest, pwd=pwd)
+            os.remove(filename)
+            if delete_src:
+                for path in src:
+                    if os.path.isdir(path):
+                        shutil.rmtree(path)
+                    else:
+                        os.remove(path)
+            return OperationResultType.SUCCEEDED
+        except:
+            try:
+                if os.path.exists(filename):
+                    os.remove(filename)
+            except:
+                pass
+            return OperationResultType.UNKNOWN_ERROR
 
     @staticmethod
     def __decompress_zipfile(src: str, dest: str) -> None:
@@ -45,23 +76,43 @@ class ZIPManager:
         pyminizip.uncompress(src, pwd, dest, 0)
 
     @staticmethod
-    def decompress(path: str, pwd: str = None) -> None:
-        if not exists(path):
-            return
+    def decompress(path: str, pwd: str = None, delete_path: bool = False) -> OperationResultType:
+        dir_name = ''
+        try:
 
-        if (index := path.rfind('.')) != -1 and path[index + 1] not in ['/', '\\']:
-            dest = path[:index] + '/'
-        else:
-            dest = path + '_/'
+            if (index := path.rfind('.')) != -1 and path[index + 1] not in ['/', '\\']:
+                dest = path[:index] + '\\'
+                if os.path.exists(dest.rstrip('\\')):
+                    dest = path[:index] + '__\\'
 
-        dir_name = generate_unique_path()
-        mkdir(dir_name)
-        ZIPManager.__decompress_pyminizip(path, dir_name, pwd)
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+            else:
+                dest = path + '_\\'
+                if os.path.exists(dest.rstrip('\\')):
+                    dest = path + '__\\'
 
-        filename = glob.glob(dir_name + '/**')[0]
+            dir_name = generate_unique_path()
+            os.mkdir(dir_name)
 
-        ZIPManager.__decompress_zipfile(filename, dest)
+            ZIPManager.__decompress_pyminizip(path, dir_name, pwd)
+            os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-        remove(filename)
-        rmdir(dir_name)
+            filename = glob.glob(dir_name + '\\**')[0]
+            ZIPManager.__decompress_zipfile(filename, dest)
+            rmtree(dir_name)
+
+            if delete_path:
+                os.remove(path)
+
+            return OperationResultType.SUCCEEDED
+        except Exception as ex:
+            try:
+                os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+                if os.path.exists(dir_name):
+                    rmtree(dir_name)
+                if str(ex).startswith('error -3 with zipfile'):  # Incorrect Password
+                    return OperationResultType.DETAILS_ERROR
+            except:
+                pass
+
+            return OperationResultType.UNKNOWN_ERROR
