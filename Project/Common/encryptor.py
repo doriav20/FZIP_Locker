44862d7e7dd2c0e8f30bf223b7f4811ae65e0cbb
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-05 01:17:42 +0200

    Update encryptor.py

--- Project/Common/encryptor.py
+++ Project/Common/encryptor.py
@@ -5,6 +5,12 @@ import os
 
 class Encryptor:
 
+    def __init__(self, key: bytes) -> None:
+        self.__key = key
+
+    def change_key(self, new_key: bytes):
+        self.__key = new_key
+
     @staticmethod
     def __pad(s: bytes) -> bytes:
         byte_add = Random.get_random_bytes(1)  # Avoiding NULLs Deletion in rstrip(b'\0')
@@ -12,26 +18,22 @@ class Encryptor:
             byte_add = Random.get_random_bytes(1)
         return s + byte_add + b'\0' * (-1 + AES.block_size - len(s) % AES.block_size)
 
-    @staticmethod
-    def __encrypt(key: bytes, bytes_content: bytes) -> bytes:
+    def __encrypt(self, bytes_content: bytes) -> bytes:
         message = Encryptor.__pad(bytes_content)
         iv = Random.get_random_bytes(AES.block_size)  # Initialization Vector
-        cipher = AES.new(key, AES.MODE_OFB, iv)  # Block Chiper Mode of Operation - Output Feedback (OFB)
+        cipher = AES.new(self.__key, AES.MODE_OFB, iv)  # Block Chiper Mode of Operation - Output Feedback (OFB)
         return iv + cipher.encrypt(message)
 
-    @staticmethod
-    def encrypt_message(key: bytes, message: str) -> bytes:
-        return Encryptor.__encrypt(key, message.encode())
+    def encrypt_message(self, message: str) -> bytes:
+        return self.__encrypt(message.encode())
 
-    @staticmethod
-    def encrypt_file(key: bytes, path: str) -> bytes:
+    def encrypt_file(self, path: str) -> bytes:
         with open(path, 'rb') as file:
             file_content = file.read()
-        return Encryptor.__encrypt(key, file_content)
+        return self.__encrypt(file_content)
 
-    @staticmethod
-    def encrypt_and_save_file(key: bytes, path: str, new_extension: str = '') -> None:
-        encrypted_content = Encryptor.encrypt_file(key, path)
+    def encrypt_and_save_file(self, path: str, new_extension: str = '') -> str:
+        encrypted_content = self.encrypt_file(path)
         os.remove(path)
 
         if new_extension:
@@ -43,31 +45,25 @@ class Encryptor:
         with open(path, 'wb') as file:
             file.write(encrypted_content)
 
-    @staticmethod
-    def __decrypt(key: bytes, encrypted_bytes_content: bytes) -> bytes:
+        return path
+
+    def __decrypt(self, encrypted_bytes_content: bytes) -> bytes:
         iv = encrypted_bytes_content[:AES.block_size]
-        cipher = AES.new(key, AES.MODE_OFB, iv)
+        cipher = AES.new(self.__key, AES.MODE_OFB, iv)
         plaintext = cipher.decrypt(encrypted_bytes_content[AES.block_size:])
         return plaintext.rstrip(b'\0')[:-1]
 
-    @staticmethod
-    def decrypt(key: bytes, encrypted_bytes_content: bytes) -> bytes:
-        return Encryptor.__decrypt(key, encrypted_bytes_content)
+    def decrypt_message(self, encrypted_message: bytes) -> str:
+        return self.__decrypt(encrypted_message).decode()
 
-    @staticmethod
-    def decrypt_message(key: bytes, encrypted_message: bytes) -> str:
-        return Encryptor.__decrypt(key, encrypted_message).decode()
-
-    @staticmethod
-    def decrypt_file(key: bytes, path: str) -> bytes:
+    def decrypt_file(self, path: str) -> bytes:
         with open(path, 'rb') as file:
             file_encrypted_content = file.read()
 
-        return Encryptor.__decrypt(key, file_encrypted_content)
+        return self.__decrypt(file_encrypted_content)
 
-    @staticmethod
-    def decrypt_and_save_file(key: bytes, path: str, old_extension: str = '') -> None:
-        decrypted_content = Encryptor.decrypt_file(key, path)
+    def decrypt_and_save_file(self, path: str, old_extension: str = '') -> str:
+        decrypted_content = self.decrypt_file(path)
         os.remove(path)
 
         if old_extension:
@@ -78,3 +74,4 @@ class Encryptor:
 
         with open(path, 'wb') as file:
             file.write(decrypted_content)
+        return path
