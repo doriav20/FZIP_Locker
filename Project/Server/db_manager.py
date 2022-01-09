624959ceef0768bb8d56cb0f4e2f0c7eff9eca67
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-09 02:11:50 +0200

    Update db_manager.py

--- Project/Server/db_manager.py
+++ Project/Server/db_manager.py
@@ -1,80 +1,93 @@
 import os
-
+import pymongo.errors
 from pymongo import MongoClient
 from Common.encryptor import Encryptor
 import pickle
 from datetime import datetime
 
+from Common.operation_result import OperationResultType
 
-cluster = MongoClient('mongodb://localhost:27017')
+cluster = MongoClient('mongodb://localhost:27017',
+                      serverSelectionTimeoutMS=5 * 1000)  # Timeout for each finding / updating
 db = cluster['users']
 collection = db['users']
 
-simple_key = b'1' * 32  # TODO key logic - CLIENT
 
-
-def register(email: str, encrypted_password: bytes, roi:np.ndarray) -> bool:
+def register(email: str, password: str) -> OperationResultType:
     try:
-        password = Encryptor.decrypt_message(simple_key, encrypted_password)
-
         creation_time = str(datetime.now())
         # Creating key depends on registration time - Importance is that each user has a different key
         new_key = pickle.dumps(creation_time)[7:39]
-        new_encrypted_password = Encryptor.encrypt_message(new_key, password)
 
-        collection.insert_one({'email': email, 'password': new_encrypted_password, 'created_time': creation_time})
-        return True
+        encryptor = Encryptor(new_key)
+        new_encrypted_password = encryptor.encrypt_text(password)
+
+        collection.insert_one({'email': email,
+                               'password': new_encrypted_password,
+                               'creation_time': creation_time})
+        return OperationResultType.SUCCEEDED
+    except pymongo.errors.DuplicateKeyError:  # Email is already in use
+        return OperationResultType.DETAILS_ERROR
+    except pymongo.errors.ServerSelectionTimeoutError:
+        print('ggg')
+        return OperationResultType.CONNECTION_ERROR
     except:  # pymongo.errors.DuplicateKeyError
-        return False
+        return OperationResultType.UNKNOWN_ERROR
 
 
-def sign_in(email: str, encrypted_password: bytes) -> bool:
-    user = collection.find_one({'email': email})
-    if user is None:
-        return False
-
+def sign_in(email: str, password: str) -> OperationResultType:
     try:
-        password = Encryptor.decrypt_message(simple_key, encrypted_password)
+        user = collection.find_one({'email': email})
+        if user is None:
+            return OperationResultType.DETAILS_ERROR  # User does not exist
 
-        creation_time = user['created_time']
+        creation_time = user['creation_time']
         key = pickle.dumps(creation_time)[7:39]
-        true_password = Encryptor.decrypt_message(key, user['password'])
 
-        return true_password == password
+        encryptor = Encryptor(key)
+        true_password, _ = encryptor.decrypt_text(user['password'])
+
+        if true_password == password:
+            return OperationResultType.SUCCEEDED
+        else:
+            return OperationResultType.DETAILS_ERROR
+    except pymongo.errors.ServerSelectionTimeoutError:
+        return OperationResultType.CONNECTION_ERROR
     except:
-        return False
+        return OperationResultType.UNKNOWN_ERROR
 
 
-def store_model(email: str, model_path: str) -> bool:
-    user = collection.find_one({'email': email})
-    if user is None:
-        return False
-
+def store_model(email: str, model_path: str) -> OperationResultType:
     try:
-        creation_time = user['created_time']
+        user = collection.find_one({'email': email})
+
+        creation_time = user['creation_time']
         key = pickle.dumps(creation_time)[7:39]
 
-        encrypted_model = Encryptor.encrypt_file(key, model_path)
+        encryptor = Encryptor(key)
+        encrypted_model = encryptor.encrypt_file(model_path)
+
         os.remove(model_path)
+
         collection.update_one(user, {'$set': {'model': encrypted_model}})
-        return True
+        return OperationResultType.SUCCEEDED
+    except pymongo.errors.ServerSelectionTimeoutError:
+        return OperationResultType.CONNECTION_ERROR
     except:
-        return False
+        return OperationResultType.UNKNOWN_ERROR
 
 
 def get_model(email: str) -> bytes:
-    user = collection.find_one({'email': email})
-    if user is None:
-        return None
     try:
-        creation_time = user['created_time']
+        user = collection.find_one({'email': email})
+        if user is None:
+            return b''
+
+        creation_time = user['creation_time']
         key = pickle.dumps(creation_time)[7:39]
 
-        model = Encryptor.decrypt(key, user['model'])
+        encryptor = Encryptor(key)
+        model, _ = encryptor.decrypt_text(user['model'], decode=False)
         return model
     except:
-        return None
-
-
-def user_exists(email: str) -> bool:
-    return collection.find_one({'email': email}) is not None
+        return b''
