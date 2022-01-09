da63e0bc993ff2e1284473264feadc520ed74d86
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-09 02:13:13 +0200

    Update face_scanner.py

--- Project/Server/face_scanner.py
+++ Project/Server/face_scanner.py
@@ -1,17 +1,20 @@
+import os
 from typing import Tuple
 
 import numpy as np
 from cv2 import cv2
-from db_manager import store_model, get_model
+
+from Common.operation_result import OperationResultType
+from Server.db_manager import get_model
 from Common.details_generator import generate_unique_id
 
 
-def create_model(email: str, images: Tuple[np.ndarray, np.ndarray, np.ndarray]) -> bool:
+def create_model(roi_3: Tuple[np.ndarray, np.ndarray, np.ndarray]) -> Tuple[OperationResultType, str]:
     try:
         x_train = []
         y_labels = []
-        for img in images:
-            x_train.append(img)
+        for roi_gray in roi_3:
+            x_train.append(roi_gray)
             y_labels.append(0)
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         recognizer.train(x_train, np.array(y_labels))
@@ -19,31 +22,37 @@ def create_model(email: str, images: Tuple[np.ndarray, np.ndarray, np.ndarray]) 
         model_path = f'./trainer_{generate_unique_id(16)}.yml'
         recognizer.save(model_path)
 
-        result = store_model(email, model_path)
-        return result
+        return OperationResultType.SUCCEEDED, model_path
     except:
-        return False
+        return OperationResultType.UNKNOWN_ERROR, ''
 
 
-def scan_with_model(email: str, image: np.ndarray) -> bool:
+def scan_with_model(email: str, roi: np.ndarray) -> OperationResultType:
+    model_path = ''
     try:
         model = get_model(email)
-        if model is None:
-            return False
-        model_path = f'./trainer_{generate_unique_id(16)}.yml'
+        if not model:
+            return OperationResultType.UNKNOWN_ERROR
 
+        model_path = f'./trainer_{generate_unique_id(16)}.yml'
         with open(model_path, 'wb') as file:
             file.write(model)
 
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         recognizer.read(model_path)
-        _, conf = recognizer.predict(image)
+        _, confidence = recognizer.predict(roi)
+        os.remove(model_path)
 
-        conf = 100 - conf
-        print(conf)
-        if conf >= 65:
-            return True
+        confidence = 100 - confidence
+        print(confidence)
+        if confidence >= 62:
+            return OperationResultType.SUCCEEDED
         else:
-            return False
+            return OperationResultType.DETAILS_ERROR
     except:
-        return False
+        try:
+            if os.path.exists(model_path):
+                os.remove(model_path)
+        except:
+            pass
+        return OperationResultType.UNKNOWN_ERROR
