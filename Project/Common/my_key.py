520c7193038e580abc9091bdd52b1224f6d65dea
doriav20 <86165796+doriav20@users.noreply.github.com>
2022-01-12 01:05:39 +0200

    Delete Project directory

--- Project/Common/my_key.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import os
-
-
-# Get common key from file called key.key
-def get_common_key() -> bytes:
-    path = os.path.dirname(os.path.abspath(__file__)) + r'\key.key'
-    key = pickle.load(open(path, mode='rb'))
-    return key
--- Project/Common/sending_datatypes.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class SendingDatatype(Enum):
-    SignIn = 0
-    Registration = 1
-    ScanFaceImage = 2
